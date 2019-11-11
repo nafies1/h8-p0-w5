7919a2a0fab191f14a5d2309132d541542a2f418
nafies1 <ghuroba.muslim@gmail.com>
2019-11-11 17:19:12 +0700

    Menyelesaikan, Week 5 Most Frequent largest number

---
+++ exercise-13.js
@@ -0,0 +1,45 @@
+ // [Exercises 13] Most Frequent Largest Numbers
+ // Logic Challenge - Most Frequent Largest Numbers
+
+function sorting(arrNumber) {
+  // code di sini
+  var angkaTerurut = arrNumber.sort((a, b) => b - a);
+  // console.log(angkaTerurut);
+  return angkaTerurut;
+}
+
+function getTotal(arrNumber) {
+  // code di sini
+  var jumlahTerbesar = [];
+  for (var i = 0; i < arrNumber.length; i++) {
+  	if (i === 0 || arrNumber[i] === arrNumber[0]){
+  		jumlahTerbesar.push(arrNumber[i]);
+  	} 
+  }
+  // console.log(jumlahTerbesar);
+  var jumlah = jumlahTerbesar.length;
+  if (jumlah === 0) {
+  	return "\'\'";
+  } else {
+  	return `angka paling besar adalah ${arrNumber[0]} dan jumlah kemunculan sebanyak ${jumlah} kali`;
+  }
+  
+}
+
+function mostFrequentLargestNumbers(arrNumber) {
+  var listSort = sorting(arrNumber);
+  var countHighest = getTotal(listSort);
+  return countHighest;
+}
+
+console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+
+console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+//'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+
+console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+
+console.log(mostFrequentLargestNumbers([]));
+//''
